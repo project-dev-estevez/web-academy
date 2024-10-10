bb238bd1b08d32bf9b1038728dd464b0505fa9ac
cateladino <cateladino16@hotmail.com>
2024-10-09 22:53:25 -0600

    Spikes nuevos

--- src/app/index/components/our-spikes/our-spikes.component.ts
+++ src/app/index/components/our-spikes/our-spikes.component.ts
@@ -29,5 +29,17 @@ export class OurSpikesComponent {
       url: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/xzBj_6cKdVg'), 
       title: 'Spike 4: Introducción a los Sistemas ERP | Estevez.Jor Academy'
     },
+    { 
+      url: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/oOqeVR-0Vrg'), 
+      title: 'Spike 5: Cloud Run | Estevez.Jor Academy'
+    },
+    { 
+      url: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/gcdlyMmQYjs'), 
+      title: 'Spike 6: Módulo de Proyectos en el ERP | Estevez.Jor Academy'
+    },
+    { 
+      url: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/mH34Z38s7jU'), 
+      title: 'Spike 7: Pruebas Unitarias | Estevez.Jor Academy'
+    },
   ];
 }
